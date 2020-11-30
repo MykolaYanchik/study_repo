15c37cbd1f42cb8172b627773caa011984d0ee2c
MykolaYanchik <mikola.yanchik@gmail.com>
2020-11-30 12:26:13 +0200

    1 commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function insert(num) {
+    document.form.textView.value = document.form.textView.value + num;
+}
+
+function clean() {
+    document.form.textView.value = "";
+}
+
+function back() {
+    var exp = document.form.textView.value;
+    document.form.textView.value = exp.substring(0, exp.length-1);
+}
+
+function equal() {
+    var i = document.form.textView.value;
+    
+    if(i)  {
+        document.form.textView.value = eval(i);
+    }
+}
+
