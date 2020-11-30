5699221efeba9586a29b6ffec24f4dcbd4a2bc17
MykolaYanchik <mikola.yanchik@gmail.com>
2020-11-30 12:59:20 +0200

    1 commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var slideIndex = 1
+
+function plusSlide(){
+    showSlides(slideIndex += 1)
+}
+
+function minusSlide(){
+    showSlides(slideIndex -= 1)
+}
+
+function curentSlide(n){
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('slider_item');
+
+    if(n > slides.length){
+        slideIndex = 1
+    }
+    if(n < 1){
+        slideIndex = slides.length
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
+showSlides(slideIndex)
